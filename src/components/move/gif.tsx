3f2f77adea8eafbabbdb0fadef6664963dfcb338
Maggie Xu <maggiemxu11@gmail.com>
2022-11-14 17:17:47 +1100

    add alt to img elem

--- src/components/move/gif.tsx
+++ src/components/move/gif.tsx
@@ -1,5 +1,5 @@
 const GIF = ({ gifLink }: { gifLink: string }): React.ReactElement => {
-  return <img className="w-full" src={gifLink}></img>;
+  return <img className="w-full" src={gifLink} alt="GIF of move"></img>;
 };
 
 export default GIF;
