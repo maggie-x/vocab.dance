c1ec0191a96c37cd4728ca861441c322e2c23ace
Maggie Xu <maggiemxu11@gmail.com>
2022-11-26 00:23:00 +1100

    add about + feedback cta

--- src/components/feedback/index.tsx
+++ src/components/feedback/index.tsx
@@ -1,15 +1,37 @@
 const Feedback = () => {
   return (
-    <div className="flex justify-center my-8 space-x-2">
-      <span>👉</span>
-      <a
-        className="text-sm underline text-orange-400"
-        href="https://rehjbjil7to.typeform.com/to/nJYZSoLN"
-        target="_blank"
-        rel="noreferrer"
-      >
-        Leave feedback
-      </a>
+    <div className="flex flex-col space-y-2 my-12">
+      <div className="text-sm  text-center text-pink-300">
+        Made with 💗 by Maggie (
+        <a
+          className="text-orange-400"
+          href="https://www.instagram.com/mmaggiex_/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          @mmaggiex_
+        </a>
+        )
+      </div>
+      <div className="text-xs text-zinc-400 text-center">
+        This passion project was created with the intention to have an easily
+        accessible and searchable dictionary of well-known dance moves and
+        foundations in hopes of aiding dancers with their training (and my own
+        training 🙈). This is very much still a WIP. If you would like to
+        suggest a move, suggest a feature, or leave constructive feedback,
+        please fill out the form below.
+      </div>
+      <div className="flex justify-center space-x-2">
+        <span>👉</span>
+        <a
+          className="text-sm underline text-orange-400"
+          href="https://rehjbjil7to.typeform.com/to/nJYZSoLN"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Leave feedback
+        </a>
+      </div>
     </div>
   );
 };
