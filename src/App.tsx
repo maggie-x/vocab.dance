a02a57cd48ec8b38d137ec6c9e7af19fb202e089
Maggie Xu <maggiemxu11@gmail.com>
2022-11-25 22:56:18 +1100

    don't use react-ga4 and use script tag, debug mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,12 @@
-import { useLocation, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import './App.css';
 import { Title } from './components/gradient-text';
 import { Layout } from './components/layout';
 import { AFRO_MOVES, HIP_HOP_MOVES, HOUSE_MOVES, Move } from './data/moves';
 import Main from './pages/main';
-import ReactGA from 'react-ga4';
-import { useEffect } from 'react';
 
 function App() {
   const { style } = useParams();
-  const location = useLocation();
-
-  useEffect(() => {
-    ReactGA.initialize('G-D2PHM9YKZR');
-    ReactGA.send({ hitType: 'pageview', page: location.pathname });
-  }, [location.pathname]);
 
   const moves = getMovesFromStyle(style);
   return (
