2579d9238b265d0d8cfffae1e8189f755e8827d8
Maggie Xu <maggiemxu11@gmail.com>
2022-12-17 16:28:37 +1100

    fix wrapping of style mode

--- src/components/style-filter/style-filter.tsx
+++ src/components/style-filter/style-filter.tsx
@@ -1,5 +1,6 @@
 import StyleLabel, { Style } from '../style';
 import clsx from 'clsx';
+import React from 'react';
 
 interface StyleFilterProps {
   handleStyleClick: (style: string) => void;
@@ -9,51 +10,59 @@ interface StyleFilterProps {
 const StyleFilter = ({ handleStyleClick, activeStyle }: StyleFilterProps) => {
   return (
     <div className="flex justify-center space-x-3">
-      <button
-        className="hover:cursor-pointer"
-        onClick={() => handleStyleClick('hiphop')}
-      >
-        <StyleLabel
-          className={clsx('text-md', {
-            'opacity-50': activeStyle !== Style.HIP_HOP,
-          })}
-          style={Style.HIP_HOP}
-        />
-      </button>
-      <button
-        className="hover:cursor-pointer"
-        onClick={() => handleStyleClick('house')}
-      >
-        <StyleLabel
-          className={clsx('text-md', {
-            'opacity-50': activeStyle !== Style.HOUSE,
-          })}
-          style={Style.HOUSE}
-        />
-      </button>
-      <button
-        className="hover:cursor-pointer"
-        onClick={() => handleStyleClick('afro')}
-      >
-        <StyleLabel
-          className={clsx('text-md', {
-            'opacity-50': activeStyle !== Style.AFRO,
-          })}
-          style={Style.AFRO}
-        />
-      </button>
-      <button
-        className="hover:cursor-pointer"
-        onClick={() => handleStyleClick('litefeet')}
-      >
-        <StyleLabel
-          className={clsx('text-md', {
-            'opacity-50': activeStyle !== Style.LITEFEET,
-          })}
-          style={Style.LITEFEET}
-        />
-      </button>
+      <StyleMode
+        handleStyleClick={handleStyleClick}
+        activeStyle={activeStyle}
+        styleURI="hiphop"
+        style={Style.HIP_HOP}
+      />
+      <StyleMode
+        handleStyleClick={handleStyleClick}
+        activeStyle={activeStyle}
+        styleURI="house"
+        style={Style.HOUSE}
+      />
+      <StyleMode
+        handleStyleClick={handleStyleClick}
+        activeStyle={activeStyle}
+        styleURI="afro"
+        style={Style.AFRO}
+      />
+      <StyleMode
+        handleStyleClick={handleStyleClick}
+        activeStyle={activeStyle}
+        styleURI="litefeet"
+        style={Style.LITEFEET}
+      />
     </div>
+  );
+};
+
+interface StyleModeProps {
+  handleStyleClick: (style: string) => void;
+  activeStyle: Style;
+  styleURI: string;
+  style: Style;
+}
+
+const StyleMode = ({
+  styleURI,
+  style,
+  handleStyleClick,
+  activeStyle,
+}: StyleModeProps): React.ReactElement => {
+  return (
+    <button
+      className="hover:cursor-pointer"
+      onClick={() => handleStyleClick(styleURI)}
+    >
+      <StyleLabel
+        className={clsx('text-sm md:text-md whitespace-nowrap', {
+          'opacity-50': activeStyle !== style,
+        })}
+        style={style}
+      />
+    </button>
   );
 };
 
